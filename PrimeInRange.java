6329d8e419e393da42f5480c15e62f3490beb7ae
user.name: "Vinit-Kumar-Jha <user.name: " Vinit-Kumar-Jha>
2025-09-25 01:49:50 +0530

    Fix: corrected misplaced braces and prime count statement

--- PrimeInRange.java
+++ PrimeInRange.java
@@ -5,5 +5,38 @@ import java.util.Scanner;
 public class PrimeInRange {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the range where you want to check: ");
+        int range = sc.nextInt();
+
+        int count = 0;
+
+        System.out.println("Prime numbers up to " + range + " are: ");
+        for (int i = 2; i <= range; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+        System.out.println("\nTotal prime numbers found: " + count);
+        sc.close();
     }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(num); i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
